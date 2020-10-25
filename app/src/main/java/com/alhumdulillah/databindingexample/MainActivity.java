6ea84bed278ed0b776db66feea5c1770bdc80e3a
AlMamun-CSE <almamuncse2020@gmail.com>
2020-10-25 21:38:19 +0600

    Initial commit

---
+++ app/src/main/java/com/alhumdulillah/databindingexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alhumdulillah.databindingexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
